9049bd550aa190f16a6ff1b38bbec3f0596dfe86
kovalevcon <kovalevcon@mail.ru>
2019-06-03 12:01:44 +0600

    Chapter 3: Added constructor for ShopProduct class

--- Controllers/ShopProductController.php
+++ Controllers/ShopProductController.php
@@ -8,12 +8,7 @@ class ShopProductController
     public function index()
     {
         /** @var ShopProduct $product */
-        $product = new ShopProduct;
-        $product->title = "Собачье сердце";
-        $product->productMainName = "Булгаков";
-        $product->productFistName = "Михаил";
-        $product->price = 5.99;
-
+        $product = new ShopProduct("Собачье сердце","Михаил","Булгаков", 5.99);
         print "Автор: {$product->getProducer()}\n";
     }
 }
--- Models/ShopProduct.php
+++ Models/ShopProduct.php
@@ -18,6 +18,22 @@ class ShopProduct
     public $price = 0;
 
     /**
+     * ShopProduct constructor.
+     *
+     * @param string $title
+     * @param string $firstName
+     * @param string $mainName
+     * @param float  $price
+     */
+    public function __construct(string $title, string $firstName, string $mainName, float $price)
+    {
+        $this->title = $title;
+        $this->productFistName = $firstName;
+        $this->productMainName = $mainName;
+        $this->price = $price;
+    }
+
+    /**
      * @return string
      */
     public function getProducer(): string
